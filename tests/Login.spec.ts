b014a2f53bdaf104f5601dded6f4c3b99182d9b7
kostiantynAnikeiev <122298136+kostiantynAnikeiev@users.noreply.github.com>
2023-07-10 17:18:38 +0300

    tests update

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -1,11 +1,12 @@
 import { test, expect } from '@playwright/test';
-
 test.describe('Login Tests', () => {
   test('should log in successfully', async ({ page }) => {
     await page.goto('https://admin.dev.hope.cloud/login?org=hcorg:dev:edVmx5QZe3p', {
       waitUntil: 'domcontentloaded',
     });
 
+    // TODO: replace with waitForEvent('mounted')
+    await page.waitForTimeout(3000);
     //login
 
     const emailInput = await page.waitForSelector('[data-testid="signin-email"] input');
